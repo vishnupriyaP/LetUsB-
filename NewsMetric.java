ac5a2af85f0d35b5eac8ed7b8825c0b432071b42
Kosko14 <z.koskovich@ou.edu>
2017-04-30 19:45:41 -0500

    Updated toString() method; added javadoc

--- NewsMetric.java
+++ NewsMetric.java
@@ -1,4 +1,13 @@
-
+/**
+ * Project 4, CS 2334, Section 010, April 19, 2017
+ * 
+ * This enumerated type specifies the type of measurement used to describe the
+ * duration of the news story, with the possibilities being length or count.
+ * 
+ * @author Zak Koskovich, Jered Little, Vishnupriya Parasaram, and Jessica Horner
+ * @version 1.0
+ * 
+ */
 public enum NewsMetric {
 
 	LENGTH,
@@ -6,7 +15,7 @@ public enum NewsMetric {
 
 	/**
 	 * <P>
-	 * This method will convert the enumeration to a prettier version.
+	 * This method will convert the enumeration to a String.
 	 * </P>
 	 */
 	@Override
@@ -19,8 +28,6 @@ public enum NewsMetric {
 			return "Count";
 		default:
 			throw new IllegalArgumentException();
-		}
-		
+		}	
 	}
-	
 }
