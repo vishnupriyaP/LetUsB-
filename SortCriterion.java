ddd7b9989ddd3d950a6da1e6b70f8f5631fabf52
Kosko14 <z.koskovich@ou.edu>
2017-04-30 19:48:41 -0500

    Updated toString() method; added javadoc

--- SortCriterion.java
+++ SortCriterion.java
@@ -1,13 +1,18 @@
-
+/**
+ * Project 4, CS 2334, Section 010, April 19, 2017
+ * 
+ * This enumerated type specifies the sort criterion for arranging new stories,
+ * with the possibilities being source, topic, subject, length, or date/time.
+ * 
+ * @author Zak Koskovich, Jered Little, Vishnupriya Parasaram, and Jessica Horner
+ * @version 1.0
+ */
 public enum SortCriterion {
-
-	
 	SOURCE,
 	TOPIC,
 	SUBJECT,
 	LENGTH,
 	DATE_TIME;
-	
 	
 	/**
 	 * <P>
@@ -16,7 +21,20 @@ public enum SortCriterion {
 	 */
 	@Override
 	public String toString() {
-		return null;
-		
+		switch(this)
+		{
+		case SOURCE:
+			return "Source";
+		case TOPIC:
+			return "Topic";
+		case SUBJECT:
+			return "Subject";
+		case LENGTH:
+			return "Length";
+		case DATE_TIME:
+			return "Date/Time";
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 }
